aa2c1157ca7aae36a61ed7e006fc327c3fca421f
mcastiello8 <129375473+mcastiello8@users.noreply.github.com>
2023-06-15 16:10:52 +0200

    Inizio progetto, modello iniziale vuoto

---
+++ src/main/java/it/prova/coge_be/CogeBeApplication.java
@@ -0,0 +1,13 @@
+package it.prova.coge_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogeBeApplication.class, args);
+	}
+
+}
